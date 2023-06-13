c7fa3e1af54e9cbce74101c6ff14d0259d71aad9
Matthew Keller <kellermatthewe@gmail.com>
2023-06-13 15:52:35 -0400

    empty page with serial port reading printing to console

---
+++ android/app/src/main/kotlin/com/example/imacs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.imacs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_libserialport/flutter_libserialport.dart';
+
+
+// import 'package:flutter_libserialport/flutter_libserialport.dart';
+
+
+void main() {
+  runApp(MyApp());
+
+  // ignore_for_file: avoid_print
+
+  print(SerialPort.availablePorts); // uncomment to see available ports or use shell command
+  SerialPort serialPort = SerialPort("/dev/cu.usbserial-210"); // ls /dev/{tty,cu}. on mac
+  serialPort.openRead();
+  while (true) {
+    print(serialPort.read(4, timeout: 1000 ));
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WARG IMACS',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
