c09b45f69a12a55c9b9a51c0f1da61481a80d77e
Matthew Keller <kellermatthewe@gmail.com>
2023-06-18 21:06:57 -0400

    mavlink library integrated and reading serial messages

---
+++ arduino/mavlink_code/mavlink_code.ino
@@ -0,0 +1,24 @@
+#include <mavlink.h>
+
+void setup() {
+  Serial.begin(57600);
+}
+
+void loop() {
+  // Create a MAVLink message buffer
+  mavlink_message_t msg;
+  uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+
+  // Create a MAVLink heartbeat message
+  mavlink_msg_heartbeat_pack(1, 200, &msg, MAV_TYPE_GENERIC, MAV_AUTOPILOT_INVALID, 0, 0, 0);
+
+  // Serialize the MAVLink message
+  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+
+  // Send the MAVLink message through the serial port
+  for (uint16_t i = 0; i < len; i++) {
+    Serial.write(buf[i]);
+  }
+  // Wait for a second
+  delay(1000);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,47 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_libserialport/flutter_libserialport.dart';
+import 'package:dart_mavlink/mavlink.dart';
+import 'package:dart_mavlink/dialects/common.dart';
+
 
 // ignore_for_file: avoid_print
 
 void main() {
-  runApp(MyApp());
-
+  print("running");
+  runApp(MyApp());  
 
   print(SerialPort.availablePorts); // uncomment to see available ports or use shell command
-  SerialPort serialPort = SerialPort("/dev/cu.usbserial-210"); // ls /dev/{tty,cu}. on mac
+  SerialPort serialPort = SerialPort("/dev/cu.usbserial-10"); // ls /dev/{tty,cu}. on mac
   serialPort.openRead();
+
+  var dialect = MavlinkDialectCommon();
+  var parser = MavlinkParser(dialect);
+
+  var sequence = 0;
+  var systemId = 255;
+  var componentId = 1;
+
   while (true) {
-    print(serialPort.read(4, timeout: 1000 ));
+    Uint8List readVal = serialPort.read(4, timeout: 1000 );
+    print("readVal: ");
+    print(readVal);
+    parser.parse(readVal);
+
+      var paramRequestList = ParamRequestList(
+        targetSystem: 1,
+        targetComponent: 1,
+      );
+      var frm = MavlinkFrame.v1(sequence, systemId, componentId, paramRequestList);
+
+      print("frm:");
+      print(frm);
+
+      print("frm.message");
+      print(frm.message);
+
+      print("frm.componentId");
+      print(frm.componentId);
   }
 }
 
