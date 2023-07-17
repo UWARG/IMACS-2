eda73e457c6c3d4e03a61f191cc517b7a1a07a32
Aaron W <aaronw23880@gmail.com>
2023-07-17 18:47:26 -0400

    initial commit to turn to class

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -9,40 +9,6 @@ import 'package:dart_mavlink/dialects/common.dart';
 
 void main() async {
 
-  var dialect = MavlinkDialectCommon();
-  var parser = MavlinkParser(dialect);
-
-  // replace this with whatever for other messages
-  parser.stream.listen((MavlinkFrame frm) {
-    if (frm.message is Attitude) {
-      var attitude = frm.message as Attitude;
-      print('Yaw: ${attitude.yaw / 3.14 * 180} [deg]');
-    }
-  });
-
-  Socket.connect("127.0.0.1", 14550).then((Socket socket){
-    print("Mission planner connected");
-
-    socket.listen(
-    // handle data from the server
-    (Uint8List data) {
-      parser.parse(data);
-    },
-
-    // handle error
-    onError: (error) {
-      print(error);
-      socket.destroy();
-    },
-
-    // ending connection
-    onDone: () {
-      print('Mission planner disconnected');
-      socket.destroy();
-    });
-
-  });
-
 }
 
 class MyApp extends StatelessWidget {
