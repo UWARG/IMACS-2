08709f43345da3ce7e961b743fd49068310f69fc
Christian Aiello <93229991+christian-aiello@users.noreply.github.com>
2023-07-23 22:34:13 -0400

    Organize code into functions

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dart_mavlink/mavlink.dart';
 import 'package:dart_mavlink/dialects/common.dart';
+import 'dart:typed_data';
 
 // ignore_for_file: avoid_print
 
@@ -28,42 +29,51 @@ void main() {
   var sequence = 0;
   var systemId = 255;
   var componentId = 1;
+  var messageId = 33; // GLOBAL_POSITION_INT
+  var interval = 1000000;
 
   // example of requesting a single instance of a message using MAV_CMD_REQUEST_MESSAGE (512)
-  var desiredMessage = 33; // GLOBAL_POSITION_INT
-  var param2 =
-      0; // parameter required for some messages, described in common message set
-  var commandLong = CommandLong(
-      targetSystem: 1,
-      targetComponent: 0,
-      command: 512,
-      confirmation: 0,
-      param1: desiredMessage.toDouble(),
-      param2: param2.toDouble(),
-      param3: 0,
-      param4: 0,
-      param5: 0,
-      param6: 0,
-      param7: 0);
-  var frm = MavlinkFrame.v1(sequence, systemId, componentId, commandLong);
-  parser.parse(frm.serialize());
+  Uint8List requestMessage(
+      int sequence, int systemId, int componentId, int messageId, int param2) {
+    var commandLong = CommandLong(
+        targetSystem: 1,
+        targetComponent: 0,
+        command: 512,
+        confirmation: 0,
+        param1: messageId.toDouble(),
+        param2: param2
+            .toDouble(), // parameter required for some messages, described in common message set
+        param3: 0,
+        param4: 0,
+        param5: 0,
+        param6: 0,
+        param7: 0);
+    var frm = MavlinkFrame.v1(sequence, systemId, componentId, commandLong);
+    return frm.serialize();
+  }
 
   // example of requesting messages at a specified rate using MAV_CMD_SET_MESSAGE_INTERVAL (511)
-  var interval = 1000000; // interval in μs
-  commandLong = CommandLong(
-      targetSystem: 1,
-      targetComponent: 0,
-      command: 511,
-      confirmation: 0,
-      param1: desiredMessage.toDouble(),
-      param2: interval.toDouble(),
-      param3: 0,
-      param4: 0,
-      param5: 0,
-      param6: 0,
-      param7: 0);
-  frm = MavlinkFrame.v1(sequence, systemId, componentId, commandLong);
-  parser.parse(frm.serialize());
+  Uint8List setMessageInterval(int sequence, int systemId, int componentId,
+      int messageId, int interval) {
+    var commandLong = CommandLong(
+        targetSystem: 1,
+        targetComponent: 0,
+        command: 511,
+        confirmation: 0,
+        param1: messageId.toDouble(),
+        param2: interval.toDouble(), // interval in μs
+        param3: 0,
+        param4: 0,
+        param5: 0,
+        param6: 0,
+        param7: 0);
+    var frm = MavlinkFrame.v1(sequence, systemId, componentId, commandLong);
+    return frm.serialize();
+  }
+
+  parser.parse(requestMessage(sequence, systemId, componentId, messageId, 0));
+  parser.parse(
+      setMessageInterval(sequence, systemId, componentId, messageId, interval));
 }
 
 class MyApp extends StatelessWidget {
