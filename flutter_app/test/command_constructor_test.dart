fc6259f98196d63decc3dabbb223e6f05572c4f6
Christian Aiello <93229991+christian-aiello@users.noreply.github.com>
2023-10-08 15:23:33 -0400

    Create message to request messages and set message intervals (#5)
    * Create and send request messages
    
    * Organize code into functions
    
    * Refactor code and change to MAVlink v2
    
    * Setup testing and write Command Constructor test
    
    * Add comments and store test results in variables
    
    * Changed variables to constants
    
    * Undelete widget test
    
    * Move dialect variable

---
+++ flutter_app/test/command_constructor_test.dart
@@ -0,0 +1,58 @@
+import 'package:dart_mavlink/mavlink.dart';
+import 'package:dart_mavlink/dialects/common.dart';
+import 'package:flutter_app/command_constructor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  /// dialect: Selected MAVLink dialect
+  /// sequence: The sequence number for the MAVLink frame.
+  /// Each component counts up its send sequence.
+  /// Allows to detect packet loss.
+  /// systemId: The MAVLink system ID of the vehicle (normally "1").
+  /// componentId: The MAVLink component ID (normally "0").
+  /// messageId: The MAVLink message ID of the requested message.
+  var dialect = MavlinkDialectCommon();
+  const sequence = 0;
+  const systemID = 1;
+  const componentID = 0;
+  const messageID = 33; // GLOBAL_POSITION_INT
+
+  group('Command Constructor Tests', () {
+    test('Request Message', () {
+      const requestMessageCommandNumber =
+          512; // MAV_CMD (MavLink Command) Number for requesting a single instance of a particular MAVLink message ID
+
+      var parser = MavlinkParser(dialect);
+      parser.stream.listen((MavlinkFrame frm) {
+        if (frm.message is CommandLong) {
+          var cl = frm.message as CommandLong;
+          expect(cl.command, equals(requestMessageCommandNumber));
+          expect(cl.param1, equals(messageID));
+        }
+      });
+
+      parser.parse(requestMessage(sequence, systemID, componentID, messageID)
+          .serialize());
+    });
+
+    test('Set Message Interval', () {
+      const interval = 1000000; // Time interval in microseconds == 1 second
+      const setIntervalCommandNumber =
+          511; // MAV_CMD (MavLink Command) Number for setting an interval between messages for a particular MAVLink message ID
+
+      var parser = MavlinkParser(dialect);
+      parser.stream.listen((MavlinkFrame frm) {
+        if (frm.message is CommandLong) {
+          var cl = frm.message as CommandLong;
+          expect(cl.command, equals(setIntervalCommandNumber));
+          expect(cl.param2, equals(interval));
+          expect(cl.param1, equals(messageID));
+        }
+      });
+
+      parser.parse(setMessageInterval(
+              sequence, systemID, componentID, messageID, interval)
+          .serialize());
+    });
+  });
+}
