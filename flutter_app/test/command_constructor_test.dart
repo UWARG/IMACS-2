d1bc47c96f891f0ab00d1912b15e39d43031ac6d
Christian Aiello <93229991+christian-aiello@users.noreply.github.com>
2023-10-08 13:30:29 -0400

    Undelete widget test

--- flutter_app/test/command_constructor_test.dart
+++ flutter_app/test/command_constructor_test.dart
@@ -4,16 +4,24 @@ import 'package:flutter_app/command_constructor.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Command Constructor Tests', () {
-    const sequence = 0;
-    const systemID = 1;
-    const componentID = 0;
-    const messageID = 33; // GLOBAL_POSITION_INT
-    var dialect = MavlinkDialectCommon();
+  /// sequence: The sequence number for the MAVLink frame.
+  /// Each component counts up its send sequence.
+  /// Allows to detect packet loss.
+  /// systemId: The MAVLink system ID of the vehicle (normally "1").
+  /// componentId: The MAVLink component ID (normally "0").
+  /// messageId: The MAVLink message ID of the requested message.
+  /// dialect: Selected MAVLink dialect
+  const sequence = 0;
+  const systemID = 1;
+  const componentID = 0;
+  const messageID = 33; // GLOBAL_POSITION_INT
+  var dialect = MavlinkDialectCommon();
 
+  group('Command Constructor Tests', () {
     test('Request Message', () {
-      const requestMessageCommandNumber = 512; // MAV_CMD (MavLink Command) Number for requesting a single instance of a particular MAVLink message ID
-      
+      const requestMessageCommandNumber =
+          512; // MAV_CMD (MavLink Command) Number for requesting a single instance of a particular MAVLink message ID
+
       var parser = MavlinkParser(dialect);
       parser.stream.listen((MavlinkFrame frm) {
         if (frm.message is CommandLong) {
@@ -29,7 +37,8 @@ void main() {
 
     test('Set Message Interval', () {
       const interval = 1000000; // Time interval in microseconds == 1 second
-      const setIntervalCommandNumber = 511; // MAV_CMD (MavLink Command) Number for setting an interval between messages for a particular MAVLink message ID
+      const setIntervalCommandNumber =
+          511; // MAV_CMD (MavLink Command) Number for setting an interval between messages for a particular MAVLink message ID
 
       var parser = MavlinkParser(dialect);
       parser.stream.listen((MavlinkFrame frm) {
