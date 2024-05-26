911b9c9c482fcb329d7e74222f8b02a2a0285f0b
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-05-26 22:19:20 +0700

    More formatting changes

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -43,31 +43,37 @@ class HomePage extends StatelessWidget {
           children: [
             // attitude
             DataField<double>(
-                name: 'Yaw (deg)',
-                value: comm.getYawStream(),
-                formatter: (double value) => (value / pi * 180.0).round()),
+              name: 'Yaw (deg)',
+              value: comm.getYawStream(),
+              formatter: (double value) => (value / pi * 180.0).round(),
+            ),
             DataField<double>(
-                name: 'Pitch (deg)',
-                value: comm.getPitchStream(),
-                formatter: (double value) => (value / pi * 180.0).round()),
+              name: 'Pitch (deg)',
+              value: comm.getPitchStream(),
+              formatter: (double value) => (value / pi * 180.0).round(),
+            ),
             DataField<double>(
-                name: 'Roll (deg)',
-                value: comm.getRollStream(),
-                formatter: (double value) => (value / pi * 180.0).round()),
+              name: 'Roll (deg)',
+              value: comm.getRollStream(),
+              formatter: (double value) => (value / pi * 180.0).round(),
+            ),
 
             // global position
             DataField<int>(
-                name: 'Latitude',
-                value: comm.getLatStream(),
-                formatter: (int value) => (value / 1e7).round()),
+              name: 'Latitude',
+              value: comm.getLatStream(),
+              formatter: (int value) => (value / 1e7).round(),
+            ),
             DataField<int>(
-                name: 'Longitude',
-                value: comm.getLonStream(),
-                formatter: (int value) => (value / 1e7).round()),
+              name: 'Longitude',
+              value: comm.getLonStream(),
+              formatter: (int value) => (value / 1e7).round(),
+            ),
             DataField<int>(
-                name: 'Altitude (m)',
-                value: comm.getAltStream(),
-                formatter: (int value) => (value / 1e3).round()),
+              name: 'Altitude (m)',
+              value: comm.getAltStream(),
+              formatter: (int value) => (value / 1e3).round(),
+            ),
           ],
         ),
       )),
