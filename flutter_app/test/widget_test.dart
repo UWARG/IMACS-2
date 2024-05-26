4550e1c215ee2a9f3bd02faa31b9d56a7462a15d
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-05-26 22:21:44 +0700

    Enabled dart analyze in run-tests.yml and removed unused imports in tests

--- flutter_app/test/widget_test.dart
+++ flutter_app/test/widget_test.dart
@@ -6,7 +6,6 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'dart:async';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
