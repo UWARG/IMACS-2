bec33990091f775bc6364ca61a0c6c98c1812a9d
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-06-04 22:19:51 +0700

    Added documentation to DataField

--- flutter_app/lib/data_field_widget.dart
+++ flutter_app/lib/data_field_widget.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 
+/// Widget to display a data stream with label and formatting.
+///
+/// This widget takes a data stream, a label for the data, and a formatting
+/// function as input. It displays the label followed by the formatted data
+/// streamed from the provided stream. If no data is available, it displays
+/// "No data".
+///
+/// @template T
+/// The type of data streamed.
 class DataField<T> extends StatelessWidget {
+  /// @brief Constructs a DataField widget.
+  ///
+  /// @param name The label to display before the data.
+  /// @param value The data stream to display.
+  /// @param formatter A function to format the streamed data before displaying.
+  ///
   const DataField(
       {Key? key,
       required this.name,
@@ -22,14 +37,15 @@ class DataField<T> extends StatelessWidget {
         ),
       ),
       StreamBuilder<T>(
-          stream: value,
-          builder: (context, snapshot) {
-            if (snapshot.hasData && snapshot.data != null) {
-              return Text(formatter(snapshot.data as T).toString());
-            } else {
-              return const Text('No data');
-            }
-          })
+        stream: value,
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            return Text(formatter(snapshot.data as T).toString());
+          } else {
+            return const Text('No data');
+          }
+        },
+      )
     ]);
   }
 }
