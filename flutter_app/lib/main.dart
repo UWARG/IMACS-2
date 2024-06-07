dd8513b252f2ce270b7bb9e457559619aff0d78e
Balaji <67008243+BalajiLeninrajan@users.noreply.github.com>
2024-06-08 00:53:26 +0700

    Refactor main.dart (#15)
    * Moved DataField to it's own file and replaced current main with placeholder
    
    * Added documentation to DataField
    
    * Added tests
    
    * Restored main
    
    * Minor style improvement
    
    ---------
    
    Co-authored-by: Balaji Leninrajan <bleninrajan@uwaterloo.ca>

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+import 'package:imacs/data_field_widget.dart';
 import 'package:imacs/mavlink_communication.dart';
 
 void main() async {
@@ -36,81 +37,43 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-          child: Container(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            // attitude
-            DataField<double>(
-              name: 'Yaw (deg)',
-              value: comm.getYawStream(),
-              formatter: (double value) => (value / pi * 180.0).round(),
-            ),
-            DataField<double>(
-              name: 'Pitch (deg)',
-              value: comm.getPitchStream(),
-              formatter: (double value) => (value / pi * 180.0).round(),
-            ),
-            DataField<double>(
-              name: 'Roll (deg)',
-              value: comm.getRollStream(),
-              formatter: (double value) => (value / pi * 180.0).round(),
-            ),
+      body: Column(
+        children: [
+          // attitude
+          DataField<double>(
+            name: 'Yaw (deg)',
+            value: comm.getYawStream(),
+            formatter: (double value) => (value / pi * 180.0).round(),
+          ),
+          DataField<double>(
+            name: 'Pitch (deg)',
+            value: comm.getPitchStream(),
+            formatter: (double value) => (value / pi * 180.0).round(),
+          ),
+          DataField<double>(
+            name: 'Roll (deg)',
+            value: comm.getRollStream(),
+            formatter: (double value) => (value / pi * 180.0).round(),
+          ),
 
-            // global position
-            DataField<int>(
-              name: 'Latitude',
-              value: comm.getLatStream(),
-              formatter: (int value) => (value / 1e7).round(),
-            ),
-            DataField<int>(
-              name: 'Longitude',
-              value: comm.getLonStream(),
-              formatter: (int value) => (value / 1e7).round(),
-            ),
-            DataField<int>(
-              name: 'Altitude (m)',
-              value: comm.getAltStream(),
-              formatter: (int value) => (value / 1e3).round(),
-            ),
-          ],
-        ),
-      )),
-    );
-  }
-}
-
-class DataField<T> extends StatelessWidget {
-  const DataField(
-      {Key? key,
-      required this.name,
-      required this.value,
-      required this.formatter})
-      : super(key: key);
-
-  final String name;
-  final Stream<T> value;
-  final Function(T) formatter;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(children: [
-      Text(
-        "$name: ",
-        style: const TextStyle(
-          fontWeight: FontWeight.bold,
-        ),
+          // global position
+          DataField<int>(
+            name: 'Latitude',
+            value: comm.getLatStream(),
+            formatter: (int value) => (value / 1e7).round(),
+          ),
+          DataField<int>(
+            name: 'Longitude',
+            value: comm.getLonStream(),
+            formatter: (int value) => (value / 1e7).round(),
+          ),
+          DataField<int>(
+            name: 'Altitude (m)',
+            value: comm.getAltStream(),
+            formatter: (int value) => (value / 1e3).round(),
+          ),
+        ],
       ),
-      StreamBuilder<T>(
-          stream: value,
-          builder: (context, snapshot) {
-            if (snapshot.hasData && snapshot.data != null) {
-              return Text(formatter(snapshot.data as T).toString());
-            } else {
-              return const Text('No data');
-            }
-          })
-    ]);
+    );
   }
 }
--- flutter_app/test/widget_test.dart
+++ flutter_app/test/widget_test.dart
@@ -9,26 +9,49 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:imacs/main.dart';
+import 'package:imacs/data_field_widget.dart';
 
 void main() {
-  testWidgets('DataField displays "No Data" when stream has no data',
-      (WidgetTester tester) async {
-    final StreamController<int> controller = StreamController<int>();
+  group('DataField widget', () {
+    testWidgets('DataField displays "No Data" when stream has no data',
+        (WidgetTester tester) async {
+      final StreamController<int> controller = StreamController<int>();
 
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: DataField<int>(
-          name: 'Test Field',
-          value: controller.stream,
-          formatter: (int value) => (value / 1e7).round(),
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: DataField<int>(
+            name: 'Test Field',
+            value: controller.stream,
+            formatter: (int value) => (value / 1e7).round(),
+          ),
         ),
-      ),
-    ));
+      ));
 
-    expect(find.text('No data'), findsOneWidget);
+      expect(find.text('No data'), findsOneWidget);
 
-    await controller.close();
+      await controller.close();
+    });
+
+    testWidgets('DataField displays formatted data from a stream',
+        (WidgetTester tester) async {
+      const label = 'Test Field';
+      final stream = Stream<int>.value(42);
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: DataField<int>(
+              name: label,
+              value: stream,
+              formatter: (int value) => value,
+            ),
+          ),
+        ),
+      );
+
+      await tester.pumpAndSettle();
+
+      expect(find.text('42'), findsOneWidget);
+    });
   });
 }
