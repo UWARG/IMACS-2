20953274804e15b2669ccdec93be42a4eddc11fa
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-06-07 23:00:24 +0700

    Minor style improvement

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -34,45 +34,46 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Column(
-          children: [
-            // attitude
-            DataField<double>(
-              name: 'Yaw (deg)',
-              value: comm.getYawStream(),
-              formatter: (double value) => (value / pi * 180.0).round(),
-            ),
-            DataField<double>(
-              name: 'Pitch (deg)',
-              value: comm.getPitchStream(),
-              formatter: (double value) => (value / pi * 180.0).round(),
-            ),
-            DataField<double>(
-              name: 'Roll (deg)',
-              value: comm.getRollStream(),
-              formatter: (double value) => (value / pi * 180.0).round(),
-            ),
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          // attitude
+          DataField<double>(
+            name: 'Yaw (deg)',
+            value: comm.getYawStream(),
+            formatter: (double value) => (value / pi * 180.0).round(),
+          ),
+          DataField<double>(
+            name: 'Pitch (deg)',
+            value: comm.getPitchStream(),
+            formatter: (double value) => (value / pi * 180.0).round(),
+          ),
+          DataField<double>(
+            name: 'Roll (deg)',
+            value: comm.getRollStream(),
+            formatter: (double value) => (value / pi * 180.0).round(),
+          ),
 
-            // global position
-            DataField<int>(
-              name: 'Latitude',
-              value: comm.getLatStream(),
-              formatter: (int value) => (value / 1e7).round(),
-            ),
-            DataField<int>(
-              name: 'Longitude',
-              value: comm.getLonStream(),
-              formatter: (int value) => (value / 1e7).round(),
-            ),
-            DataField<int>(
-              name: 'Altitude (m)',
-              value: comm.getAltStream(),
-              formatter: (int value) => (value / 1e3).round(),
-            ),
-          ],
-        ));
+          // global position
+          DataField<int>(
+            name: 'Latitude',
+            value: comm.getLatStream(),
+            formatter: (int value) => (value / 1e7).round(),
+          ),
+          DataField<int>(
+            name: 'Longitude',
+            value: comm.getLonStream(),
+            formatter: (int value) => (value / 1e7).round(),
+          ),
+          DataField<int>(
+            name: 'Altitude (m)',
+            value: comm.getAltStream(),
+            formatter: (int value) => (value / 1e3).round(),
+          ),
+        ],
+      ),
+    );
   }
 }
