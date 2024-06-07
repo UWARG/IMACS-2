d3a516ed331ed55fcb61e3b309678ae6144d5527
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-06-07 22:58:56 +0700

    Restored main

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
+import 'package:imacs/data_field_widget.dart';
 import 'package:imacs/mavlink_communication.dart';
 
 void main() async {
@@ -31,10 +34,45 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: const Placeholder(),
-    );
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          children: [
+            // attitude
+            DataField<double>(
+              name: 'Yaw (deg)',
+              value: comm.getYawStream(),
+              formatter: (double value) => (value / pi * 180.0).round(),
+            ),
+            DataField<double>(
+              name: 'Pitch (deg)',
+              value: comm.getPitchStream(),
+              formatter: (double value) => (value / pi * 180.0).round(),
+            ),
+            DataField<double>(
+              name: 'Roll (deg)',
+              value: comm.getRollStream(),
+              formatter: (double value) => (value / pi * 180.0).round(),
+            ),
+
+            // global position
+            DataField<int>(
+              name: 'Latitude',
+              value: comm.getLatStream(),
+              formatter: (int value) => (value / 1e7).round(),
+            ),
+            DataField<int>(
+              name: 'Longitude',
+              value: comm.getLonStream(),
+              formatter: (int value) => (value / 1e7).round(),
+            ),
+            DataField<int>(
+              name: 'Altitude (m)',
+              value: comm.getAltStream(),
+              formatter: (int value) => (value / 1e3).round(),
+            ),
+          ],
+        ));
   }
 }
