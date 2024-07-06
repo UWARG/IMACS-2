a0b371cdbddab733b3cabb84d590a69d6fb55873
TheFJcurve <sargunbhatti27@gmail.com>
2024-07-06 11:51:23 -0400

    added sized box for bug fix

--- flutter_app/lib/widgets/drone_information_widget.dart
+++ flutter_app/lib/widgets/drone_information_widget.dart
@@ -29,49 +29,53 @@ class DroneInformation extends StatelessWidget {
           color: Colors.black,
         ),
       ),
-      child: GridView.count(
-        crossAxisCount: 2,
+      child: SizedBox(
+        width: 500,
+        height: 400,
+        child: GridView.count(
+          crossAxisCount: 2,
 
-        /// describes how many items in one row.
-        childAspectRatio: 2,
+          /// describes how many items in one row.
+          childAspectRatio: 2,
 
-        /// describes how spaced out things are.
-        children: <DataField>[
-          DataField<double>(
-            name: 'Yaw (deg)',
-            value: getDroneInformation.getYawStream(),
-            formatter: (double value) =>
-                (value / pi * 180.0).toStringAsFixed(2),
-          ),
-          DataField<double>(
-            name: 'Pitch (deg)',
-            value: getDroneInformation.getPitchStream(),
-            formatter: (double value) =>
-                (value / pi * 180.0).toStringAsFixed(2),
-          ),
-          DataField<double>(
-            name: 'Roll (deg)',
-            value: getDroneInformation.getRollStream(),
-            formatter: (double value) =>
-                (value / pi * 180.0).toStringAsFixed(2),
-          ),
-          // global position
-          DataField<int>(
-            name: 'Latitude',
-            value: getDroneInformation.getLatStream(),
-            formatter: (int value) => (value / 1e7).toStringAsFixed(2),
-          ),
-          DataField<int>(
-            name: 'Longitude',
-            value: getDroneInformation.getLonStream(),
-            formatter: (int value) => (value / 1e7).toStringAsFixed(2),
-          ),
-          DataField<int>(
-            name: 'Altitude (m)',
-            value: getDroneInformation.getAltStream(),
-            formatter: (int value) => (value / 1e3).toStringAsFixed(2),
-          ),
-        ],
+          /// describes how spaced out things are.
+          children: <DataField>[
+            DataField<double>(
+              name: 'Yaw (deg)',
+              value: getDroneInformation.getYawStream(),
+              formatter: (double value) =>
+                  (value / pi * 180.0).toStringAsFixed(2),
+            ),
+            DataField<double>(
+              name: 'Pitch (deg)',
+              value: getDroneInformation.getPitchStream(),
+              formatter: (double value) =>
+                  (value / pi * 180.0).toStringAsFixed(2),
+            ),
+            DataField<double>(
+              name: 'Roll (deg)',
+              value: getDroneInformation.getRollStream(),
+              formatter: (double value) =>
+                  (value / pi * 180.0).toStringAsFixed(2),
+            ),
+            // global position
+            DataField<int>(
+              name: 'Latitude',
+              value: getDroneInformation.getLatStream(),
+              formatter: (int value) => (value / 1e7).toStringAsFixed(2),
+            ),
+            DataField<int>(
+              name: 'Longitude',
+              value: getDroneInformation.getLonStream(),
+              formatter: (int value) => (value / 1e7).toStringAsFixed(2),
+            ),
+            DataField<int>(
+              name: 'Altitude (m)',
+              value: getDroneInformation.getAltStream(),
+              formatter: (int value) => (value / 1e3).toStringAsFixed(2),
+            ),
+          ],
+        ),
       ),
     );
   }
