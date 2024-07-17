e5f051ae991c933d1f7f1919d096ddfe00d2a865
Sargun <sargunbhatti27@gmail.com>
2024-07-17 02:54:19 +0000

    Updates are now logged to console

--- flutter_app/lib/modules/change_drone_mode.dart
+++ flutter_app/lib/modules/change_drone_mode.dart
@@ -1,6 +1,24 @@
 import 'package:dart_mavlink/dialects/common.dart';
 import 'package:imacs/modules/mavlink_communication.dart';
 import 'package:imacs/command_constructors/set_mode_constructor.dart';
+import 'dart:developer';
+
+const String moduleName = "Change Drone Mode";
+
+/// Define the MavMode constants and their string representations.
+const Map<int, String> mavModes = {
+  mavModePreflight: "Preflight",
+  mavModeManualDisarmed: "Manual Disarmed",
+  mavModeTestDisarmed: "Test Disarmed",
+  mavModeStabilizeDisarmed: "Stabilize Disarmed",
+  mavModeGuidedDisarmed: "Guided Disarmed",
+  mavModeAutoDisarmed: "Auto Disarmed",
+  mavModeManualArmed: "Manual Armed",
+  mavModeTestArmed: "Test Armed",
+  mavModeStabilizeArmed: "Stabilize Armed",
+  mavModeGuidedArmed: "Guided Armed",
+  mavModeAutoArmed: "Auto Armed",
+};
 
 class ChangeDroneMode {
   final MavlinkCommunication comm;
@@ -15,5 +33,7 @@ class ChangeDroneMode {
     var frame = setMode(comm.sequence, systemID, componentID, baseMode);
     comm.sequence++;
     comm.write(frame);
+
+    log('[$moduleName]: ${mavModes[baseMode]} mode set.');
   }
 }
