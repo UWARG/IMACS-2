cddf268ec2761f29dd7363fcc3cbfad70559ffbf
andrewwan <andrew.y.wan06@gmail.com>
2024-10-03 13:29:09 -0400

    Partially Added a test

---
+++ flutter_app/test/widget/return_to_launch_widget_test.dart
@@ -0,0 +1,26 @@
+import 'package:dart_mavlink/dialects/common.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:imacs/modules/mavlink_communication.dart';
+import 'package:imacs/modules/return_to_launch.dart';
+import 'package:imacs/command_constructors/return_to_launch_constructor.dart';
+import 'package:imacs/widgets/return_to_launch_widget.dart';
+
+void main() {
+  group("Return to Launch Widget", () {
+    testWidgets("Return to Launch Button Generate",
+        (WidgetTester tester) async {
+      final mavlinkCommunication = MavlinkCommunication(
+          MavlinkCommunicationType.tcp, '127.0.0.1', 14550);
+      final ReturnToLaunch command = ReturnToLaunch(
+          comm: mavlinkCommunication,
+          returnToLaunchConstructor: returnToLaunch(0, 1, 0));
+
+      await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+              body: ReturnToLaunchButton(returnToLaunchCommand: command))));
+
+      expect(find.byType(ElevatedButton), findsOneWidget);
+    });
+  });
+}
