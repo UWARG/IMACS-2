54a1a8a51757ffe89e9b54330f4b50f5055dc95b
Lucas Kim <lucaskamadakim@gmail.com>
2024-11-04 20:24:27 -0500

    Add test changing port/protocol

--- flutter_app/test/widget/change_port_protocol_widget_test.dart
+++ flutter_app/test/widget/change_port_protocol_widget_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:imacs/modules/change_port_protocol.dart';
 import 'package:imacs/modules/mavlink_communication.dart';
 import 'package:imacs/widgets/change_port_protocol_widget.dart';
 
@@ -8,15 +9,13 @@ void main() {
     testWidgets(
         'PortProtocolChanger displays a dropdown menu, a button, and two input fields',
         (WidgetTester tester) async {
+      MavlinkCommunication comm = MavlinkCommunication(MavlinkCommunicationType.tcp, '127.0.0.1', 14550);
+
       await tester.pumpWidget(MaterialApp(
         home: Scaffold(
           body: PortProtocolChanger(
-            communicationAddress: '127.0.0.1',
-            communicationType: MavlinkCommunicationType.tcp,
-            tcpPort: 14550,
-            updateCommunicationParams:
-                (MavlinkCommunicationType communicationType,
-                    String communicationAddress, int tcpPort) {},
+            comm: comm,
+            changePortProtocol: ChangePortProtocol(comm: comm),
           ),
         ),
       ));
@@ -26,6 +25,41 @@ void main() {
       // Look for 3 TextFields because DropdownMenu also contains a TextField
       expect(find.byType(TextField), findsNWidgets(3));
       expect(find.byType(ElevatedButton), findsOneWidget);
+    });
+
+    testWidgets('PortProtocolChanger changes communication params',
+        (WidgetTester tester) async {
+      final comm = MavlinkCommunication(
+          MavlinkCommunicationType.tcp, '127.0.0.1', 14550);
+
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: PortProtocolChanger(
+            comm: comm,
+            changePortProtocol: ChangePortProtocol(comm: comm),
+          ),
+        ),
+      ));
+
+      // Open the dropdown menu and select a mode
+      await tester.tap(find.byType(DropdownMenu<MavlinkCommunicationType>));
+      await tester.pumpAndSettle();
+      final serialFinder = find.descendant(
+        of: find.byType(DropdownMenu<MavlinkCommunicationType>),
+        matching: find.text('serial'),
+      );
+      await tester.tap(serialFinder.first);
+      await tester.pumpAndSettle();
+
+      final textFieldFinder = find.byType(TextField);
+      await tester.enterText(textFieldFinder.at(1), "127.0.1.0");
+      await tester.enterText(textFieldFinder.at(2), "14551");
+
+      // Press the change port/protocol button
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+
+      expect(find.text('Successfully changed'), findsOneWidget);
     });
   });
 }
