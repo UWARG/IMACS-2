f590bb866e6bb025233ac661de453e59d1669f1e
andrewwan <andrew.y.wan06@gmail.com>
2025-01-17 21:02:24 -0500

    Fixed Commas

--- flutter_app/test/widget/return_to_launch_widget_test.dart
+++ flutter_app/test/widget/return_to_launch_widget_test.dart
@@ -5,52 +5,71 @@ import 'package:imacs/modules/return_to_launch.dart';
 import 'package:imacs/widgets/return_to_launch_widget.dart';
 
 void main() {
-  group("Return to Launch Widget", () {
-    testWidgets("Return to Launch Button Generate",
+  group(
+    "Return to Launch Widget",
+    () {
+      testWidgets(
+        "Return to Launch Button Generate",
         (WidgetTester tester) async {
-      final mavlinkCommunication = MavlinkCommunication(
-          MavlinkCommunicationType.tcp, '127.0.0.1', 14550);
-      final ReturnToLaunch command = ReturnToLaunch(comm: mavlinkCommunication);
+          final mavlinkCommunication = MavlinkCommunication(
+              MavlinkCommunicationType.tcp, '127.0.0.1', 14550);
+          final ReturnToLaunch command =
+              ReturnToLaunch(comm: mavlinkCommunication);
 
-      // Tests to see if the button renders
-      await tester.pumpWidget(MaterialApp(
-          home: Scaffold(
-              body: ReturnToLaunchButton(
-                  returnToLaunchCommand: command,
-                  systemID: 1,
-                  componentID: 0))));
+          // Tests to see if the button renders
+          await tester.pumpWidget(
+            MaterialApp(
+              home: Scaffold(
+                body: ReturnToLaunchButton(
+                    returnToLaunchCommand: command,
+                    systemID: 1,
+                    componentID: 0),
+              ),
+            ),
+          );
 
-      // Waits for all frames and animations to settle
-      await tester.pumpAndSettle();
-      expect(find.byType(ElevatedButton), findsOneWidget);
-      await tester.pumpAndSettle();
-      expect(find.text("Return To Launch"), findsOneWidget);
-    });
+          // Waits for all frames and animations to settle
+          await tester.pumpAndSettle();
+          expect(find.byType(ElevatedButton), findsOneWidget);
+          await tester.pumpAndSettle();
+          expect(find.text("Return To Launch"), findsOneWidget);
+        },
+      );
 
-    testWidgets("Button sends MavLink command", (WidgetTester tester) async {
-      final mavlinkCommunication = MavlinkCommunication(
-          MavlinkCommunicationType.tcp, '127.0.0.1', 14550);
-      final ReturnToLaunch command = ReturnToLaunch(comm: mavlinkCommunication);
+      testWidgets(
+        "Button sends MavLink command",
+        (WidgetTester tester) async {
+          final mavlinkCommunication = MavlinkCommunication(
+              MavlinkCommunicationType.tcp, '127.0.0.1', 14550);
+          final ReturnToLaunch command =
+              ReturnToLaunch(comm: mavlinkCommunication);
 
-      // Tests to see if the button renders
-      await tester.pumpWidget(MaterialApp(
-          home: Scaffold(
-              body: ReturnToLaunchButton(
+          // Tests to see if the button renders
+          await tester.pumpWidget(
+            MaterialApp(
+              home: Scaffold(
+                body: ReturnToLaunchButton(
                   returnToLaunchCommand: command,
                   systemID: 1,
                   componentID: 0,
-                  key: UniqueKey()))));
+                  key: UniqueKey(),
+                ),
+              ),
+            ),
+          );
 
-      await tester.pumpAndSettle();
-      expect(find.byType(ElevatedButton), findsOneWidget);
+          await tester.pumpAndSettle();
+          expect(find.byType(ElevatedButton), findsOneWidget);
 
-      await tester.tap(find.byType(ElevatedButton));
-      await tester.pumpAndSettle();
+          await tester.tap(find.byType(ElevatedButton));
+          await tester.pumpAndSettle();
 
-      // Verify the SnackBar appears then disappears
-      expect(find.text('Send Mavlink RTL command'), findsOneWidget);
-      await tester.pumpAndSettle(const Duration(seconds: 2));
-      expect(find.text('Send Mavlink RTL command'), findsNothing);
-    });
-  });
+          // Verify the SnackBar appears then disappears
+          expect(find.text('Send Mavlink RTL command'), findsOneWidget);
+          await tester.pumpAndSettle(const Duration(seconds: 2));
+          expect(find.text('Send Mavlink RTL command'), findsNothing);
+        },
+      );
+    },
+  );
 }
