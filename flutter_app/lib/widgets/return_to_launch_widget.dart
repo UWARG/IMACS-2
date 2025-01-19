7bec908d6b99cd6e23e279d49554523c177a9856
andrewwan <andrew.y.wan06@gmail.com>
2025-01-18 22:54:44 -0500

    Fixed Formatting

--- flutter_app/lib/widgets/return_to_launch_widget.dart
+++ flutter_app/lib/widgets/return_to_launch_widget.dart
@@ -7,26 +7,27 @@ class ReturnToLaunchButton extends StatelessWidget {
   final int componentID;
 
   // Needs the ReturnToLaunch object from the return_to_launch dart
-  const ReturnToLaunchButton(
-      {Key? key,
-      required this.returnToLaunchCommand,
-      required this.systemID,
-      required this.componentID})
-      : super(key: key);
+  const ReturnToLaunchButton({
+    Key? key,
+    required this.returnToLaunchCommand,
+    required this.systemID,
+    required this.componentID,
+  }) : super(key: key);
 
   // Returns a button that tells the drone to return to launch + displays a snackbar
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-        onPressed: () {
-          returnToLaunchCommand.returnNoQueue(systemID, componentID);
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('Send Mavlink RTL command'),
-              duration: Duration(seconds: 2),
-            ),
-          );
-        },
-        child: const Text("Return To Launch"));
+      onPressed: () {
+        returnToLaunchCommand.returnNoQueue(systemID, componentID);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Sent Mavlink RTL command'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+      },
+      child: const Text("Return To Launch"),
+    );
   }
 }
