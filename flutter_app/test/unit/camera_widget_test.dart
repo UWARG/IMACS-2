4cf5c57aaedb024cd395077a85c2fac4bcc0bcd0
Alex <alex.yimo.liu@gmail.com>
2025-01-23 22:16:30 -0500

    Implement test

--- flutter_app/test/unit/camera_widget_test.dart
+++ flutter_app/test/unit/camera_widget_test.dart
@@ -1,1 +1,25 @@
+import 'dart:developer';
 
+import 'package:camera_universal/camera_universal.dart';
+import 'package:test/test.dart';
+
+const String moduleName = "Camera Test";
+
+void main() {
+  test('Check Camera', () async {
+      CameraController cameraController = CameraController();
+      await cameraController.initializeCameras();
+      await cameraController.activateCamera(
+        setState: (void Function) {
+          log("[$moduleName] setState callback");
+        }, 
+        mounted: () {
+          log("[$moduleName] mounted callback");
+          return true;
+        });
+      expect(cameraController.is_camera_init, equals(true));
+      expect(cameraController.is_camera_active, equals(true));
+      
+  }
+  );
+}
